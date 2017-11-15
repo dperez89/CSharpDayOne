7529e60f098d46cba29bc592512b5a583978c481
David Perez <d.perez89@aol.com>
2017-11-15 10:12:34 -0600

    adding in introduction CalculatePay function for tutorial purposes

---
+++ CSharpDayOne/CSharpDayOne/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDayOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float hoursWorked;
+            float hourlyRate;
+            float totalPay;
+            Console.WriteLine("Please enter the number of hours worked:");
+            hoursWorked = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the pay rate:");
+            hourlyRate = float.Parse(Console.ReadLine());
+            totalPay = hoursWorked * hourlyRate;
+            Console.WriteLine("Your total pay is $" + totalPay + "!");
+            Console.ReadKey();
+        }
+    }
+}
